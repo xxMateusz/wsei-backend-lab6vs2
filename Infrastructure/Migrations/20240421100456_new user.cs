cfcb716503ee449784217df322fcf0c16c1708a5
xxMateusz <mateusz.bochenek@microsoft.wsei.edu.pl>
2024-04-23 15:17:19 +0200

    final

---
+++ Infrastructure/Migrations/20240421100456_new user.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class newuser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "Password" },
+                values: new object[] { 1, "test@gmail.com", "1234" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAnswers_QuizId",
+                table: "UserAnswers",
+                column: "QuizId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserAnswers_Quizzes_QuizId",
+                table: "UserAnswers",
+                column: "QuizId",
+                principalTable: "Quizzes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserAnswers_Users_UserId",
+                table: "UserAnswers",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserAnswers_Quizzes_QuizId",
+                table: "UserAnswers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserAnswers_Users_UserId",
+                table: "UserAnswers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserAnswers_QuizId",
+                table: "UserAnswers");
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
